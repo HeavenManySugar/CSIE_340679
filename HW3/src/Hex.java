c09bd808c2a9ba29b2e3977a9b8b17f770c69eb1
ZRE <chy853@gmail.com>
2025-03-20 11:14:17 +0800

    complete 1.2

--- HW3/src/Hex.java
+++ HW3/src/Hex.java
@@ -27,34 +27,47 @@ public class Hex {
     NOONE, BLUE, RED
   }
 
-  Vector<Player> board;
+  Vector<Vector<Player>> grid;
   int size;
+  Player currentPlayer = Player.RED;
 
   // create an empty board of size n*n
   Hex(int n) {
     size = n + 2;
-    board = new Vector<Player>(size * size);
-    for (int i = 0; i < size * size; i++) {
-      board.add(Player.NOONE);
+    grid = new Vector<Vector<Player>>();
+    for (int i = 0; i < size; i++) {
+      Vector<Player> row = new Vector<Player>();
+      for (int j = 0; j < size; j++) {
+        row.add(Player.NOONE);
+      }
+      grid.add(row);
     }
   }
 
   // return the color of cell i,j
   Player get(int i, int j) {
-    return board.get(i * size + j);
+    return grid.get(i).get(j);
   }
 
   // update the board after the player with the trait plays the cell (i, j).
   // Does nothing if the move is illegal.
   // Returns true if and only if the move is legal.
   boolean click(int i, int j) {
-    return false;
+    if (i < 0 || i > size - 1 || j < 0 || j > size - 1) {
+      return false;
+    }
+    if (get(i, j) != Player.NOONE) {
+      return false;
+    }
+    grid.get(i).set(j, currentPlayer);
+    currentPlayer = currentPlayer == Player.BLUE ? Player.RED : Player.BLUE;
+    return true;
   }
 
   // return the player with the trait or Player.NOONE if the game is over
   // because of a player's victory.
   Player currentPlayer() {
-    return Player.NOONE;
+    return currentPlayer;
   }
 
   // return the winning player, or Player.NOONE if no player has won yet
