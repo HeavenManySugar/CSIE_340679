ee63efc7b24333f8f740c5e56d93977cf01b7214
ZRE <chy853@gmail.com>
2025-04-02 15:32:52 +0800

    Finish TestCompare

--- HW5/src/KDTree.java
+++ HW5/src/KDTree.java
@@ -12,24 +12,26 @@ public class KDTree {
 	}
 
 	boolean compare(double[] a) {
-		throw(new Error("TODO"));
+		int r = this.depth % a.length;
+		if (a[r] >= this.point[r])
+			return true;
+		return false;
 	}
 
 	static KDTree insert(KDTree tree, double[] p) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static double sqDist(double[] a, double[] b) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static double[] closestNaive(KDTree tree, double[] a, double[] champion) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
-
 	static double[] closestNaive(KDTree tree, double[] a) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static double[] closest(KDTree tree, double[] a, double[] champion) {
@@ -38,29 +40,28 @@ public class KDTree {
 
 		// sert pour InteractiveClosest.
 		InteractiveClosest.trace(tree.point, champion);
-		
-		throw(new Error("TODO"));
+
+		throw (new Error("TODO"));
 	}
 
 	static double[] closest(KDTree tree, double[] a) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static int size(KDTree tree) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static void sum(KDTree tree, double[] acc) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	static double[] average(KDTree tree) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
-
 	static Vector<double[]> palette(KDTree tree, int maxpoints) {
-		throw(new Error("TODO"));
+		throw (new Error("TODO"));
 	}
 
 	public String pointToString() {
