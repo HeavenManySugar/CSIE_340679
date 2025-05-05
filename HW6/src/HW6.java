4a97108f42224291b0586a22792f8c3f19f62f76
ZRE <chy853@gmail.com>
2025-05-05 12:04:15 +0800

    Pass Test1

--- HW6/src/HW6.java
+++ HW6/src/HW6.java
@@ -14,23 +14,24 @@
  */
 
 // SINGLY 
- 
+
 class Singly<E> {
 	E element;
 	Singly<E> next;
 
-	// we choose to represent the empty list by null, the two constructors that follow cannot
+	// we choose to represent the empty list by null, the two constructors that
+	// follow cannot
 	// therefore build an empty list.
 
 	// create a list with one element.
-	
+
 	public Singly(E element, Singly<E> next) {
 		this.element = element;
 		this.next = next;
 	}
 
 	// create a list from a non-empty array.
-	
+
 	public Singly(E[] data) {
 		assert (data.length > 0) : "\nThe constructor Singly(E[] data) cannot be used with an empty array"
 				+ "\nbecause we cannot build a non-empty list without data.";
@@ -45,7 +46,7 @@ class Singly<E> {
 	}
 
 	// physical copy of a list (for testing only)
-	
+
 	static <E> Singly<E> copy(Singly<E> l) {
 		if (l == null)
 			return null;
@@ -60,7 +61,7 @@ class Singly<E> {
 	}
 
 	// test of equality of two lists
-	
+
 	static <E> boolean areEqual(Singly<E> chain1, Singly<E> chain2) {
 		while (chain1 != null && chain2 != null) {
 			if (!chain1.element.equals(chain2.element))
@@ -70,9 +71,9 @@ class Singly<E> {
 		}
 		return chain1 == chain2;
 	}
-	
+
 	// create a string from a linked list (necessary for display).
-	
+
 	public String toString() {
 		Singly<E> cursor = this;
 		String answer = "[ ";
@@ -86,18 +87,37 @@ class Singly<E> {
 
 	// Question 1
 	// Length of a list. Iterative implementation to avoid stack overflow.
-	
-	static<E> int length(Singly<E> l) {
-		throw new Error("Method length(Singly<E> l) to be completed (Question 1)");
+
+	static <E> int length(Singly<E> l) {
+		int length = 0;
+		while (l != null) {
+			length++;
+			l = l.next;
+		}
+		return length;
 	}
-	
+
 	// Question 1
 	// Cutte the second half of the list passed as an argument,
 	// the removed part is returned.
 	// The split method therefore modifies the list passed as an argument.
-	
-	static<E> Singly<E> split(Singly<E> l) {
-		throw new Error("Method split(Singly<E> l) to be completed (Question 1)");
+
+	static <E> Singly<E> split(Singly<E> l) {
+		if (l == null || l.next == null)
+			return null;
+
+		Singly<E> slow = l;
+		Singly<E> fast = l;
+
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		Singly<E> secondHalf = slow.next;
+		slow.next = null;
+
+		return secondHalf;
 	}
 }
 
@@ -106,18 +126,20 @@ class Singly<E> {
 class MergeSortString {
 
 	// Question 2.2
-	// Realizes the merge of the two lists passed as arguments, returns the merged list.
+	// Realizes the merge of the two lists passed as arguments, returns the merged
+	// list.
 	// The two lists passed as arguments are destroyed since the operation
 	// is done "in place".
-	
+
 	static Singly<String> merge(Singly<String> l1, Singly<String> l2) {
 		throw new Error("Method merge(Singly<String> l1, Singly<String> l2) to be completed (Question 2.2)");
 	}
 
 	// Question 2.2
-	// Sort (recursively) the list passed as an argument by sorting each of its two halves separately before merging the two sorted halves.
+	// Sort (recursively) the list passed as an argument by sorting each of its two
+	// halves separately before merging the two sorted halves.
 	// The list passed as an argument is destroyed during the operation.
-	
+
 	static Singly<String> sort(Singly<String> l) {
 		throw new Error("Method sort(Singly<String> l) to be completed (Question 2.2)");
 	}
@@ -134,23 +156,23 @@ class Occurrence implements Comparable<Occurrence> {
 		this.word = word;
 		this.count = count;
 	}
-	
+
 	public String toString() {
 		return word;
 	}
-	
+
 	// Question 2.3 :
 	// Return a list whose each link contains a word present
 	// in the list of words passed as an argument, with its multiplicity.
 	// The list passed as an argument can be destroyed.
-	
+
 	static Singly<Occurrence> count(Singly<String> l) {
 		throw new Error("Method count(Singly<String> l) to be completed (Question 2.3)");
 	}
-	
+
 	// Question 3.2
 	// Method of comparison necessary for the use of the sorting algorithm
-	
+
 	public int compareTo(Occurrence that) {
 		throw new Error("Method compareTo(Occurrence that) to be completed (Question 3.2)");
 	}
@@ -158,7 +180,7 @@ class Occurrence implements Comparable<Occurrence> {
 	// Question 3.2
 	// Identical to the count(Singly<String> l) method except that the returned list
 	// is sorted in descending order of multiplicity.
-	
+
 	static Singly<Occurrence> sortedCount(Singly<String> l) {
 		throw new Error("Method sortedCount(Singly<String> l) to be completed (Question 3.2)");
 	}
@@ -167,21 +189,23 @@ class Occurrence implements Comparable<Occurrence> {
 /* MERGE_SORT */
 
 // Generic version of MergeSortString
-// We replace the type "String" with the generic type "E" in the implementation of MergeSort
+// We replace the type "String" with the generic type "E" in the implementation
+// of MergeSort
 
 class MergeSort {
-	
+
 	// Question 3.1
-	// Identical to merge(Singly<String> l1, Singly<String> l2) with "E" instead of "String"
-	
-	static<E extends Comparable<E>> Singly<E> merge(Singly<E> l1, Singly<E> l2) {
+	// Identical to merge(Singly<String> l1, Singly<String> l2) with "E" instead of
+	// "String"
+
+	static <E extends Comparable<E>> Singly<E> merge(Singly<E> l1, Singly<E> l2) {
 		throw new Error("Method merge(Singly<E> l1, Singly<E> l2) to be completed (Question 3.1)");
 	}
 
 	// Question 3.1
 	// Identical to sort(Singly<String> l) with "E" instead of "String"
-	
-	static<E extends Comparable<E>> Singly<E> sort(Singly<E> l) {
+
+	static <E extends Comparable<E>> Singly<E> sort(Singly<E> l) {
 		throw new Error("Method sort(Singly<E> l) to be completed (Question 3.1)");
 	}
 
@@ -194,8 +218,8 @@ class Median {
 	// Question 3.3
 	// Returns a median of the set of numerical values passed as an argument
 	// in the form of a linked list.
-	
-	static Pair<Double> median (Singly<Double> data) {
+
+	static Pair<Double> median(Singly<Double> data) {
 		throw new Error("Method median (Singly<Double> data) to be completed (Question 3.3)");
 	}
 }
